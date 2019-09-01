8a820586780da953f7e7bee5a98df83b6b4ded54
Anthoni <Anthoni@ANTHONI_LT>
2019-09-01 18:13:22 +0600

    Custom html helper

---
+++ Library/HTMLHelpers/LabelHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.HTMLHelpers
+{
+    public static class LabelHelper
+    {
+        public static IHtmlString Create_Lable(string content, string target)
+        {
+            string LableStr = $"<label for = {target} style=\"background-color:gray;color:yellow;font-size:24px\">{content}</label>";
+            return new HtmlString(LableStr);
+        }
+    }
+}
