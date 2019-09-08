17ec3b64e7ce69cba489c81f9327749380f88bab
Anthoni <Anthoni@ANTHONI_LT>
2019-09-08 20:09:38 +0600

    AddCustomersViews

---
+++ Library/Controllers/CustomersController.cs
@@ -0,0 +1,79 @@
+﻿using Library.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            List<Customers> customers;
+            using (Model1 db = new Model1())
+            {
+                customers = db.Customers.ToList();
+            }
+            return View(customers);
+           
+        }
+        public ActionResult CreateOrEdit(int? id)
+
+        {
+            if (id == null)
+            {
+                ViewBag.Title = "Создание";
+                return View();
+            }
+            else
+            {
+                ViewBag.Title = "Редактирование";
+
+                Customers customer;
+                using (Model1 db = new Model1())
+                {
+                    customer = db.Customers.Where(a => a.id == id).FirstOrDefault();
+                }
+                return View(customer);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult CreateOrEdit(Customers customer)
+        {
+            using (Model1 db = new Model1())
+            {
+                var editableCustomer = db.Customers.Where(a => a.id == customer.id).FirstOrDefault();
+                if (editableCustomer == null)
+                {
+                    db.Customers.Add(customer);
+                }
+                else
+                {
+                    editableCustomer.fullName = customer.fullName;
+                  
+                }
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                using (Model1 db = new Model1())
+                {
+                    var deleteCustomer = db.Customers.Where(a => a.id == id).FirstOrDefault();
+                    db.Customers.Remove(deleteCustomer);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index", "Customers");
+
+        }
+    }
+}
